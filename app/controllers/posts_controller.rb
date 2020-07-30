584f70bb9b4998851fb1082d347d2b07704066de
takuya-kinoshita20160224 <takuyakinoshita20160224@gmail.com>
2020-07-30 15:03:11 +0900

    checkedアクションの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,23 @@
 class PostsController < ApplicationController
 
   def index #アクション定義
-    @posts = Post.all#インスタンス変数=モデル.メソッド
-  end
-
-  def new #アクション定義
+    @posts = Post.all.order(id: "DESC")#インスタンス変数=モデル.メソッド
   end
 
   def create #アクション定義
     Post.create(content: params[:content])#モデル.メソッド（カラム名：送信データ）
+    redirect_to action: index  #indexアクションにリダイレクト
   end
-
+  
+  def checked
+    post = Post.find(params[:id])
+    if post.checked then
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+    
+    item = Post.find(params[:id])
+    render json: { post: item }
+  end
 end
