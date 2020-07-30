492e41f415775ead910cd6d11def4dbd3654085d
takuya-kinoshita20160224 <takuyakinoshita20160224@gmail.com>
2020-07-30 20:29:02 +0900

    既読機能のフロント実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,17 +6,17 @@ class PostsController < ApplicationController
 
   def create #アクション定義
     Post.create(content: params[:content])#モデル.メソッド（カラム名：送信データ）
-    redirect_to action: index  #indexアクションにリダイレクト
+    redirect_to action: :index  #indexアクションにリダイレクト
   end
   
   def checked
-    post = Post.find(params[:id])
-    if post.checked then
-      post.update(checked: false)
-    else
-      post.update(checked: true)
+    post = Post.find(params[:id]) #指定のレコード一つをpostに代入
+    if post.checked then #レコードのcheckedカラムの値がtrue(既読)だと
+      post.update(checked: false) #checkedカラムの値をfalseにして保存
+    else #レコードのcheckedカラムの値がfalse(未読)だと
+      post.update(checked: true)  #checkedカラムの値をtrueにして保存
     end
-    
+
     item = Post.find(params[:id])
     render json: { post: item }
   end
---
+++ app/javascript/checked.js
@@ -0,0 +1,49 @@
+function check() { //関数checkを宣言
+  const posts = document.getElementsByClassName("post"); //変数postsにclass名がpostの要素を代入（投稿一つ一つ）
+  postsA = Array.from(posts); //postsAに変数postsの値を配列に変換して代入
+
+  postsA.forEach(function (post) { //postAの要素を一つずつpostに代入していく
+    if (post.getAttribute("data-load") != null) {
+      return null;
+    }
+    post.setAttribute("data-load", "true");
+    post.addEventListener("click", (e) => { //post(投稿)がクリックされると起こることを以下に記述
+
+      const postId = post.getAttribute("data-id"); //投稿一つ一つのidをpostIdに代入
+
+      const XHR = new XMLHttpRequest(); //変数XHRを定義これでXMLHttpRequestのメソッドが使用可能
+
+      XHR.open("GET", `/posts/${postId}`, true); //リクエスト指定(HTTPメソッド,パス,非同期通信)
+
+      XHR.responseType = "json"; //レスポンスの形式をjsonに指定
+
+      XHR.send(); //引数指定はなくて良い。これを記述して初めてリクエスト送信できる
+
+      XHR.onload = () => { //レスポンスが成功した時に以下が呼び出される
+        const item = XHR.response.post; //item = Post.find(params[:id])の値を変数itemに代入している
+        if (item.checked === true) { //指定したレコードのcheckedカラムの値がtrueだと
+          post.setAttribute("data-check", "true"); //data-checkの値をtrueに変更(灰色に変わる)
+        } else if (item.checked === false) { //指定したレコードのcheckedカラムの値がfalseだと
+          post.removeAttribute("data-check"); //data-check属性を取り除く
+        }
+        if (XHR.status != 200) { //レスポンスステータスが200以外(エラー)だったら
+          alert(`Error ${XHR.status}: ${XHR.statusText}`); //エラー文表示
+        } else {
+          return null; //レスポンス処理が成功だったら何もない
+        }
+      }; //ここまでがXHR.onload
+
+      XHR.onerror = () => { //もしリクエストが失敗したら以下を処理する
+        alert("Request failed"); //エラー文表示
+      };
+
+      e.preventDefault(); //addEventListener("click"をキャンセル
+
+    }); //ここまでがaddEventListener("click",
+
+  }); //ここまでがforEach文
+}
+setInterval(check, 1000); //1秒ごとに関数checkを実行(関数, 時間(ミリ秒))
+
+window.addEventListener("load", check);
+
